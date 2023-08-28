35805b01b0deebc4d9497092f9f957143258c6d0
Ole Kristian Sunde <ole.sunde@gmail.com>
2023-08-28 20:11:16 +0200

    stop appending files

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-cat "${BASH_SOURCE%/*}/zshrc" >> ~/.zshrc
-cat "${BASH_SOURCE%/*}/bash_profile" >> ~/.bash_profile
-cat "${BASH_SOURCE%/*}/bashrc" >> ~/.bashrc
+cat "${BASH_SOURCE%/*}/zshrc" > ~/.zshrc
+cat "${BASH_SOURCE%/*}/bash_profile" > ~/.bash_profile
+cat "${BASH_SOURCE%/*}/bashrc" > ~/.bashrc
