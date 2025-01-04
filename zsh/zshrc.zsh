bb0804d26a92e12b5bb28aa4a40292a433ee7b55
Ole Kristian Sunde <ole.sunde@gmail.com>
2025-01-04 09:24:33 +0100

    check that zshrc.zsh is in expected location

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,10 +1,19 @@
 # Source modular Zsh configuration files
-ZSH_CONFIG_DIR="${HOME}/Developer/PycharmProjects/mgmt/zsh"
+# Get the directory of this file (zshrc.zsh)
+CURRENT_FILE_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-# Source all files in the config folder, excluding this file.
-for config_file in "${ZSH_CONFIG_DIR}"/*.zsh; do
-  if [[ $config_file != *"/zshrc.zsh" ]]; then
-    # shellcheck source=src/$config_file
-    source "$config_file"
-  fi
-done
+# Check if the current script's directory matches ZSH_CONFIG_DIR
+if  [[ "$CURRENT_FILE_DIR" == "$ZSH_CONFIG_DIR" ]]; then
+  # Source all files in the config folder, excluding this file.
+  for config_file in "${ZSH_CONFIG_DIR}"/*.zsh; do
+    if [[ $config_file != *"/zshrc.zsh" ]]; then
+      # shellcheck source=src/$config_file
+      source "$config_file"
+    fi
+  done
+else
+  echo "zshrc.zsh is NOT in the same directory as ZSH_CONFIG_DIR!"
+  echo "zshrc.zsh is in: $CURRENT_FILE_DIR"
+  echo "ZSH_CONFIG_DIR is: $ZSH_CONFIG_DIR"
+fi
+
